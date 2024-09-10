f43e71c7796fc5fa5678be86acde35e291741dc2
6115820 <6115820@TR-BMLVT04.ead.thomsonreuters.com>
2024-09-10 12:49:51 -0300

    Adicionando consultas personalizadas com JPQL

--- src/main/java/com/educandoweb/course/controllers/ProductController.java
+++ src/main/java/com/educandoweb/course/controllers/ProductController.java
@@ -23,8 +23,10 @@ public class ProductController {
     
     // Recebe a busca paginada do frontend de todos os produtos
     @GetMapping
-    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable){
-        Page<ProductDTO> dto = service.findAll(pageable);
+    public ResponseEntity<Page<ProductDTO>> findAll(
+			@RequestParam(name = "name", defaultValue = "") String name,
+			Pageable pageable){
+        Page<ProductDTO> dto = service.findAll(name, pageable);
         return ResponseEntity.ok(dto);
     }
     
--- src/main/java/com/educandoweb/course/repositories/ProductRepository.java
+++ src/main/java/com/educandoweb/course/repositories/ProductRepository.java
@@ -2,6 +2,15 @@ package com.educandoweb.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+
 import com.educandoweb.course.entities.Product;
 
-public interface ProductRepository extends JpaRepository<Product, Long>{}
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+	@Query("SELECT obj FROM Product obj "
+			+ "WHERE UPPER(obj.name) LIKE UPPER(CONCAT('%', :name, '%'))")
+	Page<Product> searchByName(String name, Pageable pageable);
+}
